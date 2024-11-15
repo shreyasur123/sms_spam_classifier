c83b29875f473ed720d47b2bb4dbd31921112d00
Shreya Sur <96196258+shreyasur123@users.noreply.github.com>
2024-11-15 20:08:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pickle
 import nltk
-##nltk.download()
+nltk.download('wordnet')
+nltk.download('punkt')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
